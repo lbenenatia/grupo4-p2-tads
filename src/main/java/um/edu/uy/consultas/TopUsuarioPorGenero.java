613437ac2739096013a8054aa912f9e534b08072
lbenenatia <lucabenenati@outlook.com>
2025-06-24 08:50:17 -0300

    Correccion del sout del arbol respecto a generos

--- src/main/java/um/edu/uy/consultas/TopUsuarioPorGenero.java
+++ src/main/java/um/edu/uy/consultas/TopUsuarioPorGenero.java
@@ -66,7 +66,8 @@ public class TopUsuarioPorGenero {
         for (int i = 0; i < 10; i++) {
             int idUsuario = topUsuarioPorGenero[i].getKey();
             int cantEvaluaciones = topUsuarioPorGenero[i].getData();
-            System.out.println(idUsuario + ", " + top10Generos[i] + ", " + cantEvaluaciones);
+            String genero = top10Generos[i].getNombre();
+            System.out.println(idUsuario + ", " + genero + ", " + cantEvaluaciones);
         }
     }
 }
